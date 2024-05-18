386916205052050694105a03a93400a31cb854c8
epsilon <martin.olivier1997@gmail.com>
2024-05-19 00:52:34 +0200

    fixed one offs error and some bugs

--- src/klein_composer/klein_composer.py
+++ src/klein_composer/klein_composer.py
@@ -1,9 +1,13 @@
 from klein_compute.tree_explorer import tree_explorer 
+from src.klein_composer.utils import pad_to_dense
 
 import numpy as np
 import threading
 from fractions import Fraction
+from itertools import permutations
+import logging
 
+_logger = logging.getLogger(__name__)
 class KleinComposer():
     def __init__(self):
         self.gen = np.random.uniform(-1, 1, size = (4, 2, 2)) + 1.j * np.random.uniform(-1, 1, size = (4, 2, 2))
@@ -13,8 +17,34 @@ class KleinComposer():
                              [0, 2, 3, 4], 
                              [1, 0, 3, 4]])
         self.num_threads = 4
-        self.special_word = Fraction(0, 1) 
-        self.tree_exp = tree_explorer(0, 0, 3, self.gen, self.fsa)
+        self.special_fract = Fraction(0, 1) 
+        #self.word_length = special_word.p + special_word.q
+        #self.special_word = self.compute_special_word()
+        self.word_length = 0
+        self.fix_pts = [[] for i in range(4)]
+        #self.fix_pts = self.compute_fix_pts(self.special_word)
+        self.special_word = [0]
+        self.fix_pts = self.compute_fix_pts(self.special_word) #special word abAB
+
+        self.tree_exp = tree_explorer(0, 0, 3, self.gen, self.fsa, self.fix_pts)
+
+    def mobius_fixed_point(self, mat):
+        coeff = np.array([mat[1, 1], (mat[1, 1] - mat[0, 0]), -mat[0, 1]])
+        return np.roots(coeff)
+
+    def compute_fix_pts(self, special_words):
+        fix_pts = [[] for i in range(4)]
+        for spe_w in [special_words, [0, 1, 2, 3]]: 
+            for perm in set(permutations(spe_w)): 
+                word = self.gen[perm[0]]
+                for p in perm[1:]:
+                    word = np.matmul(word, self.gen[p])
+
+                idx_gen =  perm[-1]
+                for w in self.mobius_fixed_point(word).flatten().tolist():
+                    fix_pts[idx_gen].append(w)
+            
+        return pad_to_dense(fix_pts)
 
     def compute_start_points(self):
         return self.tree_exp.compute_leaf()
--- tests/unit/test_klein_composer/test_klein_composer.py
+++ tests/unit/test_klein_composer/test_klein_composer.py
@@ -1,7 +1,19 @@
 import pytest
 
+import numpy as np
 from src.klein_composer import KleinComposer
 
 def test_init():
     kc = KleinComposer()
+    assert kc.num_threads == 4
+    assert kc.word_length == 0
+    assert kc.gen.shape == (4, 2, 2)
+    assert kc.fsa.shape == (5, 4)
 
+def test_fix_pts():
+    kc = KleinComposer()
+    
+    test_word = np.matmul(kc.gen[0], kc.gen[1]) 
+    test_word = np.matmul(test_word, kc.gen[2]) 
+    test_word = np.matmul(test_word, kc.gen[3]) 
+    assert kc.mobius_fixed_point(test_word)[0] in kc.fix_pts  
