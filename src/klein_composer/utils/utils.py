4cca2b8a616345fb952fb6c6cbcd7317fa782a48
epsilon <martin.olivier1997@gmail.com>
2024-05-19 00:50:25 +0200

    created utils package

---
+++ src/klein_composer/utils/utils.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def pad_to_dense(M):
+    """Appends the minimal required amount of zeroes at the end of each 
+     array in the jagged array `M`, such that `M` looses its jagedness."""
+
+    maxlen = max(len(r) for r in M)
+
+    Z = np.zeros((len(M), maxlen), dtype = complex )
+    for enu, row in enumerate(M):
+        Z[enu, :len(row)] += row 
+    return Z
