d8ff9571d26924e9853c76988506130357d27145
epsilon <martin.olivier1997@gmail.com>
2024-05-20 17:08:45 +0200

    updated model

--- src/models/compute_model.py
+++ src/models/compute_model.py
@@ -5,3 +5,6 @@ from dataclasses import dataclass
 class ComputeModel:
     num_threads: int = 4
     max_depth: int = 3
+    start_word: np.ndarray = np.identity(2, dtype = complex)
+    start_tag: int = 0
+    start_level: int = 0
--- src/models/fractal_model.py
+++ src/models/fractal_model.py
@@ -6,5 +6,9 @@ from fractions import Fraction
 @dataclass
 class FractalModel:
     generators: np.ndarray
-    FSA: np.ndarray
+    FSA: np.ndarray = np.array([[1, 2, 3, 4], 
+                                [1, 2, 0, 4], 
+                                [1, 2, 3, 0], 
+                                [0, 2, 3, 4], 
+                                [1, 0, 3, 4]])
     special_fract: Fraction
