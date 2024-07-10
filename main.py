54b899fa669b55919a123ba481c2af2276374da3
epsilon <martin.olivier1997@gmail.com>
2024-07-10 16:27:36 +0200

    changed scopes of manager

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.klein_composer import KleinComposer
 from src.models import FractalModel, ComputeModel, OutputModel
 from src.output_manager import OutputManager
 from src.recipe_manager import RecipeManager 
+from src.fraction_manager import FractionManager 
 
 from fractions import Fraction
 import numpy as np
@@ -10,28 +11,20 @@ import logging
 import os
 
 if __name__ == "__main__":
-    rm = RecipeManager("grandma_recipe")
-    fm = rm.generate(2, 2)
     logging.basicConfig(
                 level=os.environ.get('LOGLEVEL', 'INFO').upper()
                 )
-    #fm = FractalModel(
-    #    generators=np.random.uniform(-1, 1, (4, 2, 2))
-    #    + 1.0j * np.random.uniform(-1, 1, (4, 2, 2)),
-    #    FSA=np.array(
-    #        [[1, 2, 3, 4], [1, 2, 0, 4], [1, 2, 3, 0], [0, 2, 3, 4], [1, 0, 3, 4]]
-    #    ),
-    #    special_fract=Fraction(0, 1),
-    #)
-
-    cm = ComputeModel(max_depth=10, num_threads=4)
-
-    kc = KleinComposer(fm, cm)
-
     om = OutputModel(file_type="jpg", output_directory="output")
     output_manager = OutputManager(om)
-    
+    fract_manager = FractionManager.from_farey_sequence(5)
+    cm = ComputeModel(max_depth=15, epsilon = 0.0001, num_threads=4)
 
-    p = kc.compute_thread()
+    for fm in fract_manager.generate():
 
-    output_manager.save(p)
+        kc = KleinComposer(fm, cm)
+
+        
+
+        p = kc.compute_thread()
+
+        output_manager.save(p)
