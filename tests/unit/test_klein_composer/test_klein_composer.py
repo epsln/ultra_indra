5d59aafa12aba0407151d99b5aec8eda3df52e08
epsilon <martin.olivier1997@gmail.com>
2024-07-12 13:33:43 +0200

    removed useless init test

--- tests/unit/test_klein_composer/test_klein_composer.py
+++ tests/unit/test_klein_composer/test_klein_composer.py
@@ -5,13 +5,6 @@ from src.klein_composer import KleinComposer
 from tests.conftest import fractal_model_fixture, compute_model_fixture, klein_composer_fixture
 
 
-def test_init(fractal_model_fixture, compute_model_fixture):
-    kc = KleinComposer(fractal_model_fixture, compute_model_fixture)
-    assert kc.num_threads == 4
-    assert kc.word_length == 0
-    assert kc.gen.shape == (4, 2, 2)
-    assert kc.fsa.shape == (5, 4)
-    
 def test_compute_start_depth():
     assert KleinComposer.compute_start_depth(1) == 1
     assert KleinComposer.compute_start_depth(4) == 1
